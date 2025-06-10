f6b0070cb41fb892b03d526b837feb4876eaf47a
Kartikey Singh <150163779+KartikeySinghSingraur@users.noreply.github.com>
2025-06-10 14:51:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import os
+# import os
 
 app = Flask(__name__)
 
@@ -38,8 +38,8 @@ def ask():
     except Exception as e:
         return jsonify({"reply": f"Error: {str(e)}"})
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
